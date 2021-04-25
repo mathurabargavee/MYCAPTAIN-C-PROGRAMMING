224b65ed0f3273f92daf4d62853c37be1b1cba5b
mathurabargavee <80213592+mathurabargavee@users.noreply.github.com>
2021-04-25 13:10:13 +0530

    Update Employee_Structure.c
    Write a C program that defines a structure employee containing the details such as empno, empname, department name and salary. The structure has to store 20 employees in an organization. Use the appropriate method to define the above details and define a function that will display the contents?
    
    Your Output should look similar to this
    
    Name Age Phone Number Salary Chirag 24 1234567788 20000 Arnav 31 1234567891 56000 Shivam 45 8881101111 30500

--- Employee_Structure.c
+++ Employee_Structure.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+struct Employee
+{
+    char name[20],dept[50];
+    int age;
+    long int phno;
+    float sal;
+};
+void output(struct Employee s)
+{
+    
+    printf("%s\t%d\t%ld\t%s\t%f\n",s.name,s.age,s.phno,s.dept,s.sal);
+}
+int main()
+{
+    int i;
+    struct Employee e[20];
+    for(int i=0;i<20;i++)
+       { printf("Enter Employee %d Name: ",i+1);
+        scanf("%s",e[i].name);
+        printf("Enter Employee %d Age: ",i+1);
+        scanf("%d",&e[i].age);
+        
+        printf("Enter Employee %d Phone Number: ",i+1);
+        scanf("%ld",&e[i].phno);
+       printf("Enter Employee %d Department: ", i+1);
+        scanf("%s",e[i].dept);
+        printf("Enter Employee %d salary: ",i+1);
+        scanf("%f",&e[i].sal);
+        printf("\n");
+       }
+    printf("NAME\tAGE\tPHONE NUMBER\tDEPARTMENT\t\tSALARY\n");
+    for(i=0;i<20;i++)
+    output(e[i]);
+    return 0;
+}
 
